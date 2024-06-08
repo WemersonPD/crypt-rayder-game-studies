fb9f1c56ca86bd406b9ab41070f15ba226224e33
WemersonPD <wemersondev@gmail.com>
2024-06-08 17:09:03 +0100

    feat: :sparkles: able to grab a element

--- Source/CryptRaider/Grabber.cpp
+++ Source/CryptRaider/Grabber.cpp
@@ -4,7 +4,6 @@
 
 #include "Engine/World.h"
 #include "DrawDebugHelpers.h"
-#include "PhysicsEngine/PhysicsHandleComponent.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -23,7 +22,7 @@ void UGrabber::BeginPlay()
 
 	// ...
 
-	UPhysicsHandleComponent *PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	UPhysicsHandleComponent *PhysicsHandle = GetPhysicsHandle();
 	if (PhysicsHandle == nullptr)
 	{
 		UE_LOG(LogTemp, Error, TEXT("PhysicsHandle not found on %s"), *GetOwner()->GetName());
@@ -38,15 +37,32 @@ void UGrabber::BeginPlay()
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UPhysicsHandleComponent *PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr)
+	{
+		return;
+	}
+
+	FVector ActorLocation = GetComponentLocation();
+	FVector NewLocation = ActorLocation + GetForwardVector() * HoldDistance;
+
+	PhysicsHandle->SetTargetLocationAndRotation(NewLocation, GetComponentRotation());
 }
 
 void UGrabber::Release()
 {
-	UE_LOG(LogTemp, Display, TEXT("Released Grabber!"));
+	UE_LOG(LogTemp, Display, TEXT("Released"));
 }
 
 void UGrabber::Grab()
 {
+	UPhysicsHandleComponent *PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr)
+	{
+		return;
+	}
+
 	UWorld *World = GetWorld();
 
 	FVector Start = GetComponentLocation();
@@ -65,8 +81,22 @@ void UGrabber::Grab()
 
 	if (HasHit)
 	{
-		AActor *ActorHit = HitResult.GetActor();
-		FString ActorName = ActorHit->GetActorNameOrLabel();
-		UE_LOG(LogTemp, Display, TEXT("Hitting the actor: %s"), *ActorName);
+		PhysicsHandle->GrabComponentAtLocationWithRotation(
+			HitResult.GetComponent(),
+			NAME_None,
+			HitResult.ImpactPoint,
+			GetComponentRotation());
 	}
+}
+
+UPhysicsHandleComponent *UGrabber::GetPhysicsHandle() const
+{
+	UPhysicsHandleComponent *PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (PhysicsHandle == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("PhysicsHandle not found on %s"), *GetOwner()->GetName());
+	}
+
+	return PhysicsHandle;
 }

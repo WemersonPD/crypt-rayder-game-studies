167ccdc138a127f2c6791e1aa8d4a0bc2b8993e0
WemersonPD <wemersondev@gmail.com>
2024-06-08 22:49:55 +0100

    feat: :sparkles: added trigger collision component

---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+    // Allow us to use the tick function
+    // This is useful for checking if the player is within the trigger box
+    // and then triggering an event
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTriggerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    UE_LOG(LogTemp, Display, TEXT("Trigger Component Initiated"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    AActor *KeyActor = GetAcceptableActor();
+    if (KeyActor == nullptr)
+    {
+        UE_LOG(LogTemp, Display, TEXT("Acceptable Actor Not Found"));
+
+        return;
+    }
+
+    UE_LOG(LogTemp, Display, TEXT("Acceptable Actor Found"));
+
+    // Get the Key Component to disable physics
+    UPrimitiveComponent *KeyComponent = Cast<UPrimitiveComponent>(KeyActor->GetRootComponent());
+    if (KeyComponent != nullptr)
+    {
+        KeyComponent->SetSimulatePhysics(false);
+    }
+
+    // Attach the Key Actor to the Trigger Box (A door for example)
+    KeyActor->AttachToComponent(this, FAttachmentTransformRules::KeepWorldTransform);
+
+    // Start the movement.
+    Mover->SetShouldMove(true);
+}
+
+AActor *UTriggerComponent::GetAcceptableActor() const
+{
+
+    TArray<AActor *> OverlappingActors;
+    GetOverlappingActors(OverlappingActors);
+
+    if (OverlappingActors.Num() == 0)
+    {
+        return nullptr;
+    }
+
+    for (AActor *Actor : OverlappingActors)
+    {
+
+        bool HasKeyTag = Actor->ActorHasTag(KeyTag);
+        bool IsGrabbing = Actor->ActorHasTag("Grabbed");
+
+        if (HasKeyTag && !IsGrabbing)
+        {
+            return Actor;
+        }
+    }
+
+    return nullptr;
+}
+
+void UTriggerComponent::SetMover(UMover *NewMover)
+{
+    Mover = NewMover;
+}
